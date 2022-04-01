4915e16ae8352422b369a6b2365a140559a8c7dc
Kimon Kukulis <kimon.kukulis@gmail.com>
2022-04-01 22:12:26 +0300

    functions: rgbTohex, float2int, sleep added to utisl

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,8 @@
+import Color from "./colors";
+
 export const ensure = <T>(
   argument: T | undefined | null,
-  message = "This value was promised to be there.",
+  message = "This value was promised to be there."
 ): T => {
   if (argument === undefined || argument === null) {
     throw new TypeError(message);
@@ -9,6 +11,19 @@ export const ensure = <T>(
   return argument;
 };
 
-export const rgbToHex = (r: number, g: number, b: number): string => {
-  return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+export const rgbToHex = (color: Color): string => {
+  return (
+    "#" +
+    ((1 << 24) + (color.r << 16) + (color.g << 8) + color.b)
+      .toString(16)
+      .slice(1)
+  );
+};
+
+export const float2int = (value: number): number => {
+  return value >> 0;
+};
+
+export const sleep = (ms: number) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 };
