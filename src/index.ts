05807e748047605c1cc30a5470b31752f073acb2
Kimon Kukulis <kimon.kukulis@gmail.com>
2022-04-01 22:14:22 +0300

    move player, draw line (bresenham algorithm), fix issues with PutPixel function, handle keys

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import "@/index.scss";
 import Player from "./player";
 import Color from "./utils/colors";
-import { ensure, rgbToHex } from "./utils";
+import Map from "./map";
+import { ensure, float2int, rgbToHex, sleep } from "./utils";
 
 export class Game {
   canvas: Element;
@@ -10,40 +11,190 @@ export class Game {
   width: number;
   height: number;
   player: Player;
+  map: Map;
+
+  keyLeft: boolean;
+  keyRight: boolean;
+  keyUp: boolean;
+  keyDown: boolean;
+
+  ctxID: ImageData;
 
   constructor() {
     this.canvas = ensure(document.querySelector("#screen"));
     this.ctx = ensure((this.canvas as HTMLCanvasElement).getContext("2d"));
+
     this.width = 1024;
     this.height = 512;
 
-    this.player = new Player();
+    this.ctxID = this.ctx.getImageData(0, 0, this.width, this.height);
+    this.keyLeft = false;
+    this.keyRight = false;
+    this.keyUp = false;
+    this.keyDown = false;
+
+    this.player = new Player(300, 300);
+    this.map = new Map();
+
+    document.addEventListener(
+      "keydown",
+      (event) => {
+        const keyName = event.key;
+
+        if (keyName === "a") this.keyLeft = true;
+        if (keyName === "d") this.keyRight = true;
+        if (keyName === "w") this.keyUp = true;
+        if (keyName === "s") this.keyDown = true;
+
+        // console.log(`Key pressed ${keyName}`);
+      },
+      false
+    );
+
+    document.addEventListener(
+      "keyup",
+      (event) => {
+        const keyName = event.key;
+
+        if (keyName === "a") this.keyLeft = false;
+        if (keyName === "d") this.keyRight = false;
+        if (keyName === "w") this.keyUp = false;
+        if (keyName === "s") this.keyDown = false;
+
+        // console.log(`Key released ${keyName}`);
+      },
+      false
+    );
   }
 
-  clear(color: Color): void {
-    this.ctx.fillStyle = rgbToHex(color.r, color.g, color.b);
+  async Clear(color: Color): Promise<void> {
+    this.ctx.fillStyle = rgbToHex(color);
     this.ctx.fillRect(0, 0, this.width, this.height);
   }
 
-  putPixel(x: number, y: number, color: Color): void {
-    const id = this.ctx.getImageData(0, 0, this.width, this.height);
-    const off = (y * id.width + x) * 4;
-    const pixels = id.data;
-
+  PutPixel(x: number, y: number, color: Color): void {
+    const off = (y * this.ctxID.width + x) * 4;
+    const pixels = this.ctxID.data;
     pixels[off] = color.r;
     pixels[off + 1] = color.g;
     pixels[off + 2] = color.b;
     pixels[off + 3] = 255;
-
-    this.ctx.putImageData(id, 0, 0);
   }
 
-  run(): void {
-    // console.log("Game is running");
-    this.clear(new Color(64, 64, 64));
-    this.putPixel(32, 32, new Color(255, 0, 0));
+  FetchImage() {
+    this.ctxID = this.ctx.getImageData(0, 0, this.width, this.height);
+  }
+
+  UpdateImage() {
+    this.ctx.putImageData(this.ctxID, 0, 0);
+  }
+
+  DrawRect(x: number, y: number, w: number, h: number, color: Color) {
+    this.ctx.fillStyle = rgbToHex(color);
+    this.ctx.fillRect(x, y, w, h);
+  }
+
+  async DrawPlayer() {
+    const color = new Color(255, 200, 0);
+    const size = 8;
+    this.DrawRect(
+      this.player.x - size,
+      this.player.y - size,
+      size * 2,
+      size * 2,
+      color
+    );
+
+    this.DrawLine(this.player.x, this.player.y, this.player.x + (this.player.px*100), this.player.y + (this.player.py*100), new Color(255, 0, 0));
+  }
+
+  DrawLine(x0: number, y0: number, x1: number, y1: number, color: Color) {
+    this.FetchImage();
+
+    x0 = float2int(x0);
+    y0 = float2int(y0);
+    x1 = float2int(x1);
+    y1 = float2int(y1);
+
+    const dx = Math.abs(x1 - x0);
+    const dy = Math.abs(y1 - y0);
+    const sx = x0 < x1 ? 1 : -1;
+    const sy = y0 < y1 ? 1 : -1;
+    let err = dx - dy;
+    color.b = 0;
+
+    while (true) {
+      this.PutPixel(x0, y0, color);
+
+      if (x0 === x1 && y0 === y1) break;
+      const e2 = 2 * err;
+      if (e2 > -dy) {
+        err -= dy;
+        x0 += sx;
+      }
+      if (e2 < dx) {
+        err += dx;
+        y0 += sy;
+      }
+    }
+
+    this.UpdateImage();
+  }
+
+  async DrawMap2d() {
+    for (let y = 0; y < this.map.mapY; y++) {
+      for (let x = 0; x < this.map.mapX; x++) {
+        const tileID = this.map.data[y * this.map.mapX + x];
+        let color = new Color(0, 0, 0);
+
+        if (tileID === 1) {
+          color = new Color(255, 255, 255);
+        }
+
+        this.DrawRect(
+          x * this.map.mapS + 1,
+          y * this.map.mapS + 1,
+          this.map.mapS - 1,
+          this.map.mapS - 1,
+          color
+        );
+      }
+    }
+  }
+
+  async HandleInputs() {
+    const movementSpeed = 2;
+    const turningSpeed = 0.1;
+
+    if (this.keyLeft) {
+      this.player.AddRotation(-turningSpeed);
+    }
+    if (this.keyRight) {
+      this.player.AddRotation(turningSpeed);
+    }
+    if (this.keyUp) {
+      this.player.x += this.player.px * movementSpeed;
+      this.player.y += this.player.py * movementSpeed;
+    }
+    if (this.keyDown) {
+      this.player.x -= this.player.px * movementSpeed;
+      this.player.y -= this.player.py * movementSpeed;
+    }
+  }
+
+  async Run() {
+    console.log("Game is running");
+    while (true) {
+      this.Clear(new Color(64, 64, 64));
+      this.DrawMap2d();
+      this.DrawPlayer();
+
+      // await console.log(`x ${this.player.x}, y: ${this.player.y}`);
+      this.HandleInputs();
+      await sleep(16);
+    }
   }
 }
 
 export const game = new Game();
-game.run();
+game.Run();
--- src/player.ts
+++ src/player.ts
@@ -1,8 +1,25 @@
 export default class Player {
   x = 0;
   y = 0;
+  px = 0;
+  py = 0;
+  angle = 0;
   constructor(x = 0, y = 0) {
     this.x = x;
     this.y = y;
+    this.UpdateDeltas();
+  }
+
+  UpdateDeltas() {
+    this.px = Math.cos(this.angle);
+    this.py = Math.sin(this.angle);
+  }
+
+  AddRotation(turningAmount: number) {
+    this.angle += turningAmount;
+    if (this.angle > Math.PI * 2) this.angle -= Math.PI * 2;
+    if (this.angle < 0) this.angle += Math.PI * 2;
+
+    this.UpdateDeltas();
   }
 }
